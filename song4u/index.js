22dcb0b75d7f73297f22f58ff6170e204418fb63
Israel Yarce <israelhuerta1227@gmail.com>
2022-06-25 18:22:14 -0500

    added age and generation

--- song4u/index.js
+++ song4u/index.js
@@ -1,4 +1,5 @@
 const querystring = require('qs');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -6,9 +7,68 @@ module.exports = async function (context, req) {
     const queryObject = querystring.parse(req.body);
 
     const url = queryObject.MediaUrl0;
-    context.log(url);
+
+    const resp = await fetch(url, {
+        method: "GET",
+    })
+
+    const data = await resp.arrayBuffer();
+
+    const result = analyzeImage(data);
+
+    context.log(result);
+
+    const age = result[0].faceAttributes.age;
+
+    let id = "";
+
+    if(age > 5 && age < 25) {
+        id = 'GenZ';
+    }
+    else if(age > 24 && age < 41) {
+        id = 'GenY';
+    }
+    else if(age > 40 && age < 57) {
+        id = 'GenX';
+    }
+    else if(age > 56 && age < 76) {
+        id = 'BabyBoomers';
+    }
+    else {
+        id = 'Unknown';
+    }
+
+    context.log(id);
+
+    //context.log(url);
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: url,
+        body: id,
     };
+}
+
+async function analyzeImage(img, context) {
+    const KEY = process.env["FACE_API_KEY"];
+    const URI_BASE = process.env["FACE_API_URI"] + "/face/v1.0/detect";
+
+    let params = new URLSearchParams({
+        returnFaceId: "true",
+        returnFaceAttributes: "age",
+    });
+
+    //context.log(URI_BASE +  "?" + params.toString());
+
+
+    const resp = await fetch(URI_BASE + "?" + params.toString(), {
+        method: 'POST',
+        body: img,
+        headers: {
+            'CONTENT-TYPE': 'application/octet-stream',
+            "Ocp-Apim-Subscription-Key": KEY
+        }
+    });
+
+    const data = await resp.json();
+
+    return data;
 }
