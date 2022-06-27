e2a755f6651218f6c4e80aedd25a76e0d04b6f75
Israel Yarce <israelhuerta1227@gmail.com>
2022-06-26 19:32:12 -0500

    added songs that go based of generation

--- song4u/index.js
+++ song4u/index.js
@@ -14,7 +14,7 @@ module.exports = async function (context, req) {
 
     const data = await resp.arrayBuffer();
 
-    const result = await analyzeImage(data);
+    const result = await analyzeImage(data, context);
 
     context.log(result);
 
@@ -38,15 +38,23 @@ module.exports = async function (context, req) {
         id = "Unknown";
     }
 
-    context.log(id);
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+        "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+        "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+        "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+        "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
+    const song = songs[id];
+
+    const message = `We guessed you're part of this generation: ${id}! Happy listening! ${song}`;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: id
+        body: message,
     };
 }
 
-async function analyzeImage(img){
+async function analyzeImage(img, context){
     const KEY = process.env["SUBSCRIPTIONKEY"];
     const URI_BASE = process.env["ENDPOINT"] + "/face/v1.0/detect";
 
