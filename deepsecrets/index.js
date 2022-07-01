3980640adee43b9ebc530b30b7dd3ec0dec35f27
Israel Yarce <israelhuerta1227@gmail.com>
2022-07-01 13:24:11 -0500

    return all secrets randomly instead of most recent

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -47,7 +47,7 @@ async function createDocument(newItem) {
 
     // query to return all items
     const querySpec = {
-    query: "SELECT top 1 * FROM c order by c._ts desc"
+        query: "SELECT * from c"
     };
 
     // read all items in the Items container
@@ -68,8 +68,9 @@ module.exports = async function (context, req) {
     let message = queryObject.Body; //This is the user's input
     let document = {"message": message} // create an object with the string `"message"` as the key, and the variable `message` as its value
     let items =  await createDocument(document); // call the createDocument function with the document we just made
+    let random_value = Math.floor(items.length * Math.random());
 
-    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[0].message)}`
+    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[random_value].message)}`
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
